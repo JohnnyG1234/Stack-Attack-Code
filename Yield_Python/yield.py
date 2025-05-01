ab1957bf58797d8c3c80ebb826b2bbe44e5718ca
John Groton <john.groton@mymail.champlain.edu>
2025-04-30 21:35:33 -0400

    final code

--- Yield_Python/yield.py
+++ Yield_Python/yield.py
@@ -1,35 +1,35 @@
-
-# This is a  generator!
-def func():
-    yield "Hello"
-    yield "World"
-
-# calling that generator
-def helloWorld():
-    gen = func()
-    print(next(gen))
-    print(next(gen))
+import fileinput
 
 
-# another  generator
-def digitsOfPi():
-    pi = "3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128"
+# A normal function
+def getPi():
+    with open("pi.txt", "r") as file:
+        return [line.rstrip() for line in file]
 
-    for digit in pi:
-        yield digit
 
-def getDigits(numDigits: int):
-    digits = ""
-    pi = digitsOfPi()
 
-    for i in range(numDigits):
-        digits += next(pi)
+#print(getPi())
     
-    return float(digits)
 
-if __name__ == "__main__":
-    helloWorld()
 
-    print(getDigits(5))
-    print(getDigits(15))
-    print(getDigits(1))
+
+
+
+# A generator
+def piGenerator():
+    with open("pi.txt", "r") as file:
+        for line in file:
+            yield line.rstrip()
+    
+
+
+genPi = piGenerator()
+
+print(next(genPi))
+print(next(genPi))
+print(next(genPi))
+print(next(genPi))
+print(next(genPi))
+
+
+
