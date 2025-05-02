8d5b4f343d2a78b749049152f5a0c31323cf8be1
John Groton <john.groton@mymail.champlain.edu>
2025-05-02 12:38:29 -0400

    working on tern op

---
+++ ternaryop/ternary.py
@@ -0,0 +1,17 @@
+
+
+def main():
+    num = 5
+
+    isEven = True if num % 2 == 0 else False
+
+
+def isEven(num):
+    return True if num % 2 == 0 else False
+
+
+
+
+
+if __name__ == "__main__":
+    main()
