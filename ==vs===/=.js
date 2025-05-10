ebe5b19b027275b4057dcbe2a7f3091d6d752f3b
John Groton <john.groton@mymail.champlain.edu>
2025-05-10 16:02:28 -0400

    adding == js code

---
+++ ==vs===/=.js
@@ -0,0 +1,10 @@
+var  stringNum = "5";
+
+var nunNum = 5;
+
+console.log("checking with ==");
+console.log(stringNum == nunNum);
+
+
+console.log("checking with ===");
+console.log(stringNum === nunNum);
