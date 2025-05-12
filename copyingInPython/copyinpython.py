16bbfdfec695d1ec86ad5e13ff2968176b754589
John Groton <john.groton@mymail.champlain.edu>
2025-05-12 10:49:28 -0400

    final change to copy

--- copyingInPython/copyinpython.py
+++ copyingInPython/copyinpython.py
@@ -1,11 +1,12 @@
 # How to clone a list without changing the original list in Python
 from copy import deepcopy
+
 cool_list = [1, 2, 3, 4, 5]
 
-#print(cool_list)
+
 
 other_list = cool_list[:]
 
-other_list.append(6)
+other_list.append(1)
 
-print(other_list)
+print(cool_list)
