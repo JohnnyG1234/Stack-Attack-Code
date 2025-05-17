fbb69e81c149509c1f0f74215f02f4b640077480
John Groton <john.groton@mymail.champlain.edu>
2025-05-17 16:53:25 -0400

    final change for datetime

--- dateTimeInPython/dateTime.py
+++ dateTimeInPython/dateTime.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+time = datetime.now().date()
 
-other = datetime.now().time()
+now = datetime.now().strftime('%Y-%m-%d %H')
 
-print(other)
+print(now)
