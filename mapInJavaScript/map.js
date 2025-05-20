03158025c7bbee5b28167fa73e2f8854970df8d3
John Groton <john.groton@mymail.champlain.edu>
2025-05-19 21:03:13 -0400

    final change for map in js

--- mapInJavaScript/map.js
+++ mapInJavaScript/map.js
@@ -1,9 +1,12 @@
+const words = ['Hello', 'World', 'foo', 'bar'];
+
 function toLower(word) {
   return word.toLowerCase();
 }
 
-const words = ['Hello', 'World', 'foo', 'bar'];
-
+const lowerWords2 = words.map(toLower);
 const lowerWords = words.map(x => x.toLowerCase());
 
+console.log(words);
 console.log(lowerWords);
+console.log(lowerWords2);
