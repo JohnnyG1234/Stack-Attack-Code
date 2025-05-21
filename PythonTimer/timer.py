621a6f51168226818a7db4031a2a37c40548ecdc
John Groton <john.groton@mymail.champlain.edu>
2025-05-21 11:12:20 -0400

    finishing timer code

--- PythonTimer/timer.py
+++ PythonTimer/timer.py
@@ -1,10 +1,26 @@
 import time
+from copy import  deepcopy
+
+
+my_list = [1, 2, 3, 4, 5]
 
 start = time.time()
-
-
-
+new_list =  my_list[:]
+new_list =  my_list[:]
+new_list =  my_list[:]
+new_list =  my_list[:]
+new_list =  my_list[:]
 end = time.time()
-length = end - start
-
+length = end   - start
 print(length)
+
+start1 = time.time()
+new_list2 = deepcopy(my_list)
+new_list2 = deepcopy(my_list)
+new_list2 = deepcopy(my_list)
+new_list2 = deepcopy(my_list)
+new_list2 = deepcopy(my_list)
+end1 = time.time()
+length1 = end1   - start1
+
+print(length1)
